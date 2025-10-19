926efc1cfaec5cfb958d6dc4fd2925c130b79723
Madhan <madhankumar200015@gmail.com>
2025-10-19 09:09:24 +0530

    Delete privateRoutes directory

--- privateRoutes/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import  userReducer from  "../redux/userSlice"
-const store = configureStore({
-  reducer: {
-    userInfo:userReducer  },
-}); 
-
-export default store;
